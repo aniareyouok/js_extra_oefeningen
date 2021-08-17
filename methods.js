c4de7cabb6993f35170b282de6f0b9bd97df17fb
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-08-17 15:37:21 +0200

    exercise 1a done with push() and join()

--- methods.js
+++ methods.js
@@ -5,8 +5,14 @@ const emailadresses = ['n.eeken@novi.nl', 'n.stuivenberg@novi.nl', 'm.vandergees
 /* Opdracht 1 */
 // 1a. Onze nieuwe medewerker Melissa moet worden toegevoegd aan de lijst met emailadressen. Haar email is: m.westerbroek@novi.nl.
 
+console.log('\nExercise 1a\n-------------');
+
+emailadresses.push('m.westerbroek@novi.nl');
+console.log(`E-mailaddressen van medewerkers: \n${emailadresses.join(", ")}`);
+
 // 1b. Er zullen ongetwijfeld vaker emailadressen toegevoegd moeten worden. Binnen het bedrijf zijn er echter lijsten voor medewerkers,
 // maar ook voor studenten. Schrijf een herbruikbare functie die een email-lijst en nieuw-emailadres verwacht en deze vervolgens toevoegt aan die lijst.
+
 
 /* Opdracht 2 */
 // 2a. Jouw collega wil weten of het emailadres van Nick Stuivenberg in de lijst staat. Zoek dit voor hem uit! Zijn emailadres is n.stuivenberg@novi.nl
